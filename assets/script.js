3c4c62ad8c0f52bd1a976137cbe6a11c9cb9a424
TJ Draper <tj@buzzingpixel.com>
2017-10-06 15:21:31 -0500

    Switch code from tabs to spaces

--- assets/script.js
+++ assets/script.js
@@ -1,118 +1,118 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	// Set first and last slides
-	var firstSlide = 1;
-	var lastSlide = 84;
+    // Set first and last slides
+    var firstSlide = 1;
+    var lastSlide = 84;
 
-	// Set slides director
-	var dir = 'slides';
+    // Set slides director
+    var dir = 'slides';
 
-	// Set image extension
-	var ext = 'jpg';
+    // Set image extension
+    var ext = 'jpg';
 
-	// Get the hash
-	var hash = parseInt(window.location.hash.substring(1));
+    // Get the hash
+    var hash = parseInt(window.location.hash.substring(1));
 
-	// Get the slide element
-	var $slide = $('.js-slide');
+    // Get the slide element
+    var $slide = $('.js-slide');
 
-	// Set slide
-	var setSlide = function(num) {
-		// Save the int for the new hash
-		var newHash = parseInt(num);
+    // Set slide
+    var setSlide = function(num) {
+        // Save the int for the new hash
+        var newHash = parseInt(num);
 
-		// Make sure num is an int
-		num = parseInt(num);
+        // Make sure num is an int
+        num = parseInt(num);
 
-		// Now parse the num as a string
-		num = String(num);
+        // Now parse the num as a string
+        num = String(num);
 
-		// If length is only 1, add two leading zeroes
-		if (num.length == 1) {
-			num = '00' + num;
-		}
+        // If length is only 1, add two leading zeroes
+        if (num.length == 1) {
+            num = '00' + num;
+        }
 
-		// If length is 2, add 1 leading zero
-		if (num.length == 2) {
-			num = '0' + num;
-		}
+        // If length is 2, add 1 leading zero
+        if (num.length == 2) {
+            num = '0' + num;
+        }
 
-		// Set the slide background
-		$slide.css(
-			'background-image',
-			'url(' + dir + '/' + num + '.' + ext + ')'
-		);
+        // Set the slide background
+        $slide.css(
+            'background-image',
+            'url(' + dir + '/' + num + '.' + ext + ')'
+        );
 
-		// Set the location hash
-		window.location.hash = num;
+        // Set the location hash
+        window.location.hash = num;
 
-		// Update the hash variable
-		hash = newHash;
-	};
+        // Update the hash variable
+        hash = newHash;
+    };
 
-	// Var prev function
-	var prev = function() {
-		// Set the previous slide
-		var slide = hash - 1;
+    // Var prev function
+    var prev = function() {
+        // Set the previous slide
+        var slide = hash - 1;
 
-		// Make sure the slide is not out of range
-		if (slide >= firstSlide) {
-			// Set the slide
-			setSlide(slide);
-		}
-	};
+        // Make sure the slide is not out of range
+        if (slide >= firstSlide) {
+            // Set the slide
+            setSlide(slide);
+        }
+    };
 
-	// Var advance function
-	var advance = function() {
-		// Set the next slide
-		var slide = hash + 1;
+    // Var advance function
+    var advance = function() {
+        // Set the next slide
+        var slide = hash + 1;
 
-		// Make sure the slide is not out of range
-		if (slide <= lastSlide) {
-			// Set the slide
-			setSlide(slide);
-		}
-	};
+        // Make sure the slide is not out of range
+        if (slide <= lastSlide) {
+            // Set the slide
+            setSlide(slide);
+        }
+    };
 
-	// Make sure there is a hash
-	if (! hash) {
-		hash = firstSlide;
-	}
+    // Make sure there is a hash
+    if (! hash) {
+        hash = firstSlide;
+    }
 
-	// Make sure hash is not less than first slide
-	if (hash < firstSlide) {
-		hash = firstSlide;
-	}
+    // Make sure hash is not less than first slide
+    if (hash < firstSlide) {
+        hash = firstSlide;
+    }
 
-	// Make sure hash is not greater than last slide
-	if (hash > lastSlide) {
-		hash = lastSlide;
-	}
+    // Make sure hash is not greater than last slide
+    if (hash > lastSlide) {
+        hash = lastSlide;
+    }
 
-	// Set slide background
-	setSlide(hash);
+    // Set slide background
+    setSlide(hash);
 
-	// Watch for previous click
-	$('.js-slide-prev').on('click', function() {
-		prev();
-	});
+    // Watch for previous click
+    $('.js-slide-prev').on('click', function() {
+        prev();
+    });
 
-	// Watch for next click
-	$('.js-slide-advance').on('click', function() {
-		advance();
-	});
+    // Watch for next click
+    $('.js-slide-advance').on('click', function() {
+        advance();
+    });
 
-	// Watch for keyup
-	$('body').on('keyup', function(e) {
-		if (e.keyCode === 39) { // advance (right arrow)
-			advance();
-		} else if (e.keyCode === 40) { // advance (down arrow)
-			advance();
-		} else if (e.keyCode === 37) { // prev (left arrow)
-			prev();
-		} else if (e.keyCode === 38) { // prev (up arrow)
-			prev();
-		}
-	});
+    // Watch for keyup
+    $('body').on('keyup', function(e) {
+        if (e.keyCode === 39) { // advance (right arrow)
+            advance();
+        } else if (e.keyCode === 40) { // advance (down arrow)
+            advance();
+        } else if (e.keyCode === 37) { // prev (left arrow)
+            prev();
+        } else if (e.keyCode === 38) { // prev (up arrow)
+            prev();
+        }
+    });
 })();
